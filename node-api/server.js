b47f55cc3cb2ae0305aca826f72e4b6ea93cb11e
shadowdeveloper <ro_web1@hotmail.com>
2020-02-14 00:14:24 -0300

    Criando model de produto e inserindo no banco

--- node-api/server.js
+++ node-api/server.js
@@ -1,15 +1,28 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const requireDir = require('require-dir')
 
 // Iniciando o App
 const app = express();
 
 // Iniciando o DB
-mongoose.connect('mongodb://localhost:27017/nodeapi', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect('mongodb://localhost:27017/nodeapi', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+requireDir('./src/models/');
+
+const Product = mongoose.model('Product');
 
 // Primeira rota
 app.get('/', (req, res) => {
-    res.send("Hello World");
+    Product.create({
+        title: 'React Native',
+        description: 'Build native apps with React',
+        url: 'http://github.com/facebook.com/react-native'
+    })
+    return res.send("Hello World");
 });
 
 app.listen(3001);
